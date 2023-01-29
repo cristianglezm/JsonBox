9b18cbb9c37025c2a7c02095f72c036f6d2539b7
Cristian Gonzalez <cristian.glez.m@gmail.com>
2023-01-29 19:47:07 +0100

    Update Android.mk
    added  debug version for android JsonBox_d and JsonBox_d-static

--- src/Android.mk
+++ src/Android.mk
@@ -1,6 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
+LOCAL_MODULE := JsonBox_d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libJsonBox_d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := JsonBox
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libJsonBox.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
@@ -16,6 +29,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := JsonBox-static
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libJsonBox.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_STATIC_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := JsonBox_d-static
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libJsonBox_d.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
 prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
